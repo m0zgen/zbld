cd0d75a580e2c40866828d6e0fff790d2c9c9880
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2024-01-13 19:45:49 +0600

    + + Cache comments for code

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-// CacheEntry структура для хранения кэшированных записей
+// CacheEntry cache entries structure
 type CacheEntry struct {
 	IPv4         net.IP
 	IPv6         net.IP
@@ -15,30 +15,34 @@ type CacheEntry struct {
 	TTL          time.Duration
 }
 
-// Cache структура для хранения кэша
+// Cache structure for storing cache entries
 type Cache struct {
 	mu    sync.RWMutex
 	Store map[string]CacheEntry
 }
 
-// GlobalCache глобальная переменная для кэша
+// GlobalCache global cache instance
 var GlobalCache = Cache{
 	Store: make(map[string]CacheEntry),
 }
 
+// RLock mutex lock
 func (cache *Cache) RLock() {
 	cache.mu.RLock()
 }
 
+// RUnlock mutex unlock
 func (cache *Cache) RUnlock() {
 	cache.mu.RUnlock()
 }
 
+// UpdateCreationTimeWithTTL update cache entry TTL and creation time
 func (entry *CacheEntry) UpdateCreationTimeWithTTL(ttl time.Duration) {
 	entry.CreationTime = time.Now()
 	entry.TTL = ttl
 }
 
+// CheckAndDeleteExpiredEntries check and delete expired TTL entries from cache
 func CheckAndDeleteExpiredEntries() {
 	// Check and delete expired TTL entries from cache
 	GlobalCache.mu.Lock()
